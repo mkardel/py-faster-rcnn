ae573d60044af617b9c793e22fac2c137ea4cba2
Matthias Kardel <mkardel@gmail.com>
2017-03-20 10:58:13 +1300

    changed to fit resolution

--- lib/fast_rcnn/config.py
+++ lib/fast_rcnn/config.py
@@ -35,10 +35,14 @@ __C.TRAIN = edict()
 
 # Scales to use during training (can list multiple scales)
 # Each scale is the pixel size of an image's shortest side
-__C.TRAIN.SCALES = (600,)
+#__C.TRAIN.SCALES = (600,)
+# Changed to get better results on 720p images
+__C.TRAIN.SCALES = (720,)
 
 # Max pixel size of the longest side of a scaled input image
-__C.TRAIN.MAX_SIZE = 1000
+#__C.TRAIN.MAX_SIZE = 1000
+# Changed to get better results on 720p images
+__C.TRAIN.MAX_SIZE = 1280
 
 # Images to use per minibatch
 __C.TRAIN.IMS_PER_BATCH = 2
@@ -132,10 +136,14 @@ __C.TEST = edict()
 
 # Scales to use during testing (can list multiple scales)
 # Each scale is the pixel size of an image's shortest side
-__C.TEST.SCALES = (600,)
+#__C.TEST.SCALES = (600,)
+# Changed to get better results on 720p images
+__C.TEST.SCALES = (720,)
 
 # Max pixel size of the longest side of a scaled input image
-__C.TEST.MAX_SIZE = 1000
+#__C.TEST.MAX_SIZE = 1000
+# Changed to get better results on 720p images
+__C.TEST.MAX_SIZE = 1280
 
 # Overlap threshold used for non-maximum suppression (suppress boxes with
 # IoU >= this threshold)
